4dcaa5b10a9e88e00e86e03ba28918db678a12ce
Ian Riley <ian.riley.915@gmail.com>
2015-07-21 12:21:20 -0700

    Initial build configurations and project setup.

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+/// <reference path="./typings/node/node.d.ts" />
+var format = require('gulp-clang-format');
+var gulp = require('gulp');
+var gulpClean = require('gulp-clean');
+var gulpTsc = require('gulp-typescript');
+
+
+// ==========
+// config
+
+// Sets typescript compilation options using tsconfig.json.
+var tsProject = gulpTsc.createProject('./tsconfig.json', {
+    typescript: require('typescript')
+  });
+  
+
+// ==========
+// format
+
+/**
+ * Checks that all files in modules match the format specified in .clang-format.
+ */
+gulp.task('check-format', function() {
+  return gulp.src('./modules/**/*.ts')
+    .pipe(format.checkFormat('file'))
+    .on('warning', function(e) { process.stdout.write(e.message); process.exit(1) });
+});
+
+
+// ==========
+// compile
+
+gulp.task('!clean', function() {
+  return gulp.src('./dist', {read: false}).pipe(gulpClean());
+});
+
+/**
+ * Transcompile all TypeScript code to JavaScript.
+ */
+gulp.task('build', ['!clean'], function() {
+  var tsResult = tsProject.src().pipe(gulpTsc(tsProject));
+  return tsResult.js.pipe(gulp.dest(tsProject.options.outDir));
+});
+
