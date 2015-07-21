ffc23a0381f7c2f831e7f752a0a8aac849cb9b48
Ian Riley <ian.riley.915@gmail.com>
2015-07-21 14:19:57 -0700

    Bootstraps an initial test component, following from angular.io quick start guide.

---
+++ app.ts
@@ -0,0 +1,17 @@
+/// <reference path="typings/angular2/angular2.d.ts" />
+import {Component, View, bootstrap} from 'angular2/angular2';
+
+@Component({
+  selector: 'my-app'
+})
+@View({
+  template: '<h1>Hello {{ name }}</h1>'
+})
+class MyAppComponent {
+  name: string;
+
+  constructor() {
+    this.name = 'Alice';
+  }
+}
+bootstrap(MyAppComponent);
